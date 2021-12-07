26d9e08e21341d12ce3e9bad5c195a9d29183f54
Espi <joshespi@gmail.com>
2021-12-07 13:46:15 -0700

    WO82892

--- pcsdtwentysixteen/page-email-form.php
+++ pcsdtwentysixteen/page-email-form.php
@@ -23,20 +23,44 @@
 						<label for="subject">Subject: </label>
 						<input id="subject" name="subject" <?php if(!empty($prefillsubject)) { echo 'value="' . $prefillsubject . '"';} ?>>
 						<label for="message">Message: </label>
-						<textarea id="message" name="message" placeholder="What would you like to say..." required><?php if(!empty($prefillmessage)) { echo $prefillmessage;} ?></textarea>
+						<textarea id="message" name="message" minlength="15" placeholder="What would you like to say..." required><?php if(!empty($prefillmessage)) { echo $prefillmessage;} ?></textarea>
 						<input type="checkbox" name="carbon" <?php if(!empty($prefillcarbon)) { echo 'checked';} ?>><p>Send a copy to my address</p>
 						<label for="staff_id"></label><input type="hidden" id="staff_id" name="staff_id" value="<?php echo $staff_member; ?>" readonly>
-						<input type="submit" value="Send Message" class="g-recaptcha">
-						
-						
+						<input type="checkbox" name="sanity" id="sanity" class="hidden" unchecked><label for="sanity" class="hidden">sanity check</label>
+						<input type="submit" value="Send Message" class="g-recaptcha" >
+
+						<script>
+							jQuery("#emailForm").submit(function(e) {
+								if(!jQuery('#sanity:unchecked').length) {
+									alert("Are you a robot?");
+									//stop the form from submitting
+									return false;
+								}
+								return true;
+							});
+						</script>
+
 						<!--
-						<button type="submit" class="g-recaptcha" 
-						data-sitekey="6LckCL0ZAAAAAGx733fhC7SB8zd3s_aczlGdlnvT" 
-						data-callback='onSubmit' 
-						data-action='submit'>Send Message
-					</button>
+
+						<input class="g-recaptcha" data-sitekey="6LckCL0ZAAAAAGx733fhC7SB8zd3s_aczlGdlnvT" data-callback='onSubmit' value="Send Message" type="submit">
+						<script src="https://www.google.com/recaptcha/api.js"></script>
+
+												<script>
+													   function onSubmit(token) {
+														 document.getElementById("emailForm").submit();
+													   }
+													   function onClick(e) {
+														   e.preventDefault();
+														   grecaptcha.ready(function() {
+															 grecaptcha.execute('6LckCL0ZAAAAAGx733fhC7SB8zd3s_aczlGdlnvT', {action: 'submit'}).then(function(token) {
+																 // Add your logic to submit to your backend server here.
+															 });
+														   });
+														 }
+												 </script>
 						-->
 					</form>
+
 				<div class="clear"></div>
    			</article>
    		</section>
