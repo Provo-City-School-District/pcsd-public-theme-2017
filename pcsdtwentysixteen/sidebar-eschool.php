b6593425d0c02eb194770c567d7573a9aca0d735
Espi <joshespi@gmail.com>
2022-03-28 15:28:56 -0600

    added eschool address

--- pcsdtwentysixteen/sidebar-eschool.php
+++ pcsdtwentysixteen/sidebar-eschool.php
@@ -36,5 +36,10 @@
 			<li>Fax: (801) 374-4996</li>
 			<li>Email: eschool@provo.edu</li>
 		</ul>
+		<p>Address:</p>
+			<ul>
+				<li>1591 N Jordan Ave</li>
+				<li>Provo, UT 84604</li>
+			</ul>
 		<a href="https://www.peachjar.com/index.php?region=151161&a=28&b=138"><img src="https://westridge.provo.edu/wp-content/uploads/2019/05/button-orange-eflyers_202x46.png" alt="Link to PeachJar Fliers"></a>
 </aside>
