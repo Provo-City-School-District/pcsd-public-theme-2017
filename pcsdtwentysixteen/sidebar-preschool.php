99a72015600a93af9651389f8259f32a447a5cea
Espi <joshespi@gmail.com>
2022-03-29 08:45:54 -0600

    case87904

--- pcsdtwentysixteen/sidebar-preschool.php
+++ pcsdtwentysixteen/sidebar-preschool.php
@@ -13,7 +13,11 @@
 				<?php
 					echo file_get_contents('https://provo.edu/directory_page/school-preschool-sunrise/');
 				?>
-			<h3><a class="ext" href="https://provohigh.provo.edu/school-info/baby-bulldog-preschool/"><strong>Baby Bulldog Preschool (PHS)</strong></a></h3>
+			<h3><a class="ext" href="https://provo.edu/preschool/title-i-preschool-registration/"><strong>Provo City School District Title I Preschool</strong></a></h3>
+				<?php
+					echo file_get_contents('https://provo.edu/directory_page/school-preschool-district-title-i/');
+				?>
+			<h3><a class="ext" href="https://drive.google.com/file/d/1zCvHG_Zzy-qX7JG8969mlMxCEadz13AO/view"><strong>Baby Bulldog Preschool (PHS)</strong></a></h3>
 				<?php
 					echo file_get_contents('https://provo.edu/directory_page/school-preschool-baby-bulldog/');
 				?>
@@ -21,9 +25,4 @@
 				<?php
 					echo file_get_contents('https://provo.edu/directory_page/school-preschool-ths-preschool/');
 				?>
-			
-			<h3><a class="ext" href="https://provo.edu/preschool/title-i-preschool-registration/"><strong>Provo City School District Title I Preschool</strong></a></h3>
-				<?php
-					echo file_get_contents('https://provo.edu/directory_page/school-preschool-district-title-i/');
-				?>	
 </aside>
