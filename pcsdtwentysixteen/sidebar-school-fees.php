bd7b96949431fb34c4ddc4e95b08333f74ecca2f
Espi <joshespi@gmail.com>
2022-07-05 10:08:23 -0600

    updated icons and forms on the "School Fees" sidebar

--- pcsdtwentysixteen/sidebar-school-fees.php
+++ pcsdtwentysixteen/sidebar-school-fees.php
@@ -8,73 +8,102 @@
 			// close cURL resource, and free up system resources
 			curl_close($i_am_buttons);
 		?>
-
+		
 	<ul class="imagelist">
 		<li>
 			<a href="https://provo.edu/school-fees/">
-				<img src="//globalassets.provo.edu/image/icons/business-lt.svg" alt="" />
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/light/employee-access.svg" alt="" />
 				<span>School Fees Menu</span>
 			</a>
 		</li>
 		<li>
 			<a href="https://provo.edu/wp-content/uploads/2021/02/Policy-6160-School-Fees-and-Fee-Waivers-revised-1-7-2021.pdf">
-				<img src="//globalassets.provo.edu/image/icons/policy.svg" alt="" />
-				<span>6160 School Fees and Fee Waivers</span>
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/light/register.svg" alt="" />
+				<span>6160 School Fees and Fee Waivers Policy</span>
 			</a>
 		</li>
 		<li>
 			<a href="https://provo.edu/wp-content/uploads/2021/02/6160-P1-School-Fees-and-Fee-Waivers-revised-1-7-2021.pdf">
-				<img src="//globalassets.provo.edu/image/icons/policy.svg" alt="" />
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/light/register.svg" alt="" />
 				<span>6160 Procedure 1 School Fees and Fee Waivers</span>
 			</a>
 		</li>
-		<li>
-			<a href="https://provo.edu/wp-content/uploads/2020/02/6160FeesNoticeK-6.pdf">
-				<img src="//globalassets.provo.edu/image/icons/policy.svg" alt="" />
-				<span>6160 Fee Notice (Grade K-6)</span>
-			</a>
-		</li>
-		<li>
-			<a href="https://provo.edu/wp-content/uploads/2020/02/6160FeesNotice7-12.pdf">
-				<img src="//globalassets.provo.edu/image/icons/policy.svg" alt="" />
-				<span>6160 Fee Notice (Grade 7-12)</span>
-			</a>
-		</li>
-		<li>
-			<a href="https://provo.edu/wp-content/uploads/2022/06/6160-F1-FeeWaiverApplicationK6.pdf">
-				<img src="//globalassets.provo.edu/image/icons/policy.svg" alt="" />
-				<span>6160 Form 1 Fee Waiver Application (Grade K-6)</span>
-			</a>
-		</li>
-		<li>
-			<a href="https://provo.edu/wp-content/uploads/2022/06/6160-F1-FeeWaiverApplication712.pdf">
-				<img src="//globalassets.provo.edu/image/icons/policy.svg" alt="" />
-				<span>6160 Form 1 Fee Waiver Application (Grade 7-12)</span>
-			</a>
-		</li>
-		<li>
+		<li lang="es">
 			<a href="https://provo.edu/wp-content/uploads/2021/01/Spanish-Policy-6160-School-Fees-and-Fee-Waivers.pdf">
-				<img src="//globalassets.provo.edu/image/icons/policy.svg" alt="" />
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/light/register.svg" alt="" />
 				<span>6160 Tarifas Escolares y Tarifas Exentas</span>
 			</a>
 		</li>
 		<li>
-			<a href="https://provo.edu/wp-content/uploads/2022/06/6160-F1-FeeWaiverApplicationK6Spanish.pdf">
-				<img src="//globalassets.provo.edu/image/icons/policy.svg" alt="" />
-				<span>6160 Solicitud de Exención de Cuotas (Grados K-6)</span>
-			</a>
-		</li>
-		<li>
-			<a href="https://provo.edu/wp-content/uploads/2022/06/6160-F1-FeeWaiverApplication712Spanish.pdf">
-				<img src="//globalassets.provo.edu/image/icons/policy.svg" alt="" />
-				<span>6160 Solicitud de Exoneracion de Cuotas (Grados 7-12)</span>
-			</a>
-		</li>
-		<li>
 			<a href="https://provo.edu/wp-content/uploads/2020/02/6160-F2-Fee-Waiver-Decision-And-Appeal.pdf">
-				<img src="//globalassets.provo.edu/image/icons/policy.svg" alt="" />
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/light/register.svg" alt="" />
 				<span>6160 Form 2 Fee Waiver Decision And Appeal</span>
 			</a>
 		</li>
 	</ul>
+	
+	<h2>Grades K-6</h2>
+		<ul class="imagelist">
+			<li>
+				<a href="https://provo.edu/wp-content/uploads/2022/06/march22-FeesNoticeK6.pdf">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/light/policies.svg" alt="" />
+					<span>School Fees Notice</span>
+				</a>
+			</li>
+			<li>
+				<a href="https://provo.edu/wp-content/uploads/2022/06/march22-FeeWaiverApplicationK6.pdf">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/light/register.svg" alt="" />
+					<span>School Fee Waiver Application</span>
+				</a>
+			</li>
+			
+			
+			
+			
+			<li lang="es">
+				<a href="https://provo.edu/wp-content/uploads/2022/06/march2022-FeesNoticeK6Spanish.pdf">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/light/register.svg" alt="" />
+					<span>Aviso de Cuotas Escolares Para Familias de Estudiantes</span>
+				</a>
+			</li>
+			<li lang="es">
+				<a href="https://provo.edu/wp-content/uploads/2022/06/march22-FeeWaiverApplicationK6Spanish.pdf">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/light/register.svg" alt="" />
+					<span>Solicitud de Exención de Cuotas</span>
+				</a>
+			</li>
+		</ul>
+	
+	<h2>Grades 7-12</h2>
+		<ul class="imagelist">
+			<li>
+				<a href="https://provo.edu/wp-content/uploads/2022/06/march22-FeesNotice712.pdf">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/light/policies.svg" alt="" />
+					<span>School Fees Notice</span>
+				</a>
+			</li>
+			<li>
+				<a href="https://provo.edu/wp-content/uploads/2022/06/march22-FeeWaiverApplication712.pdf">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/light/register.svg" alt="" />
+					<span>School Fee Waiver Application</span>
+				</a>
+			</li>
+			
+			
+			
+			
+			<li lang="es">
+				<a href="https://provo.edu/wp-content/uploads/2022/06/march2022-FeesNotice712Spanish.pdf">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/light/register.svg" alt="" />
+					<span>Aviso de Cuotas Escolares Para Familias de Estudiantes</span>
+				</a>
+			</li>
+			<li lang="es">
+				<a href="https://provo.edu/wp-content/uploads/2022/06/march22-FeeWaiverApplication712Spanish.pdf">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/light/register.svg" alt="" />
+					<span>Solicitud de Exención de Cuotas</span>
+				</a>
+			</li>
+		</ul>
+
 </aside>
